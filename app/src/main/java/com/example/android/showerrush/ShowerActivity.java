33b7d13183dcec80adcf3e2a7c1ef560b6e84c9e
Abraham Silva <abraham@tenatek.com>
2019-02-14 04:06:51 -0600

    added chronometer

--- app/src/main/java/com/example/android/showerrush/ShowerActivity.java
+++ app/src/main/java/com/example/android/showerrush/ShowerActivity.java
@@ -1,13 +1,48 @@
 package com.example.android.showerrush;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Chronometer;
 
 public class ShowerActivity extends AppCompatActivity {
+
+    private Chronometer chronometer;
+    private boolean running;
+    private long pauseOffset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shower);
+
+        chronometer = findViewById(R.id.chronometer);
+
+    }
+
+    public void startChrono(View view){
+
+        if(!running){
+            chronometer.setBase(SystemClock.elapsedRealtime()-pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+
+    }
+    public void pauseChrono(View view){
+
+        if(running){
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+
+    }
+    public void resetChrono(View view){
+
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
     }
 }
