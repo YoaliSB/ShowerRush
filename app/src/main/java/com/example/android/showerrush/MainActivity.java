6f004e25c3dc29cbde7c4f2038d08750e45639ef
Yoali <sb.yoali@gmail.com>
2019-02-15 13:28:26 -0600

    reading and writing to local JSON

--- app/src/main/java/com/example/android/showerrush/MainActivity.java
+++ app/src/main/java/com/example/android/showerrush/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.showerrush;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 
@@ -15,8 +17,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -31,12 +40,20 @@ public class MainActivity extends AppCompatActivity {
     private List<Shower> showers;
     private RecyclerView recyclerView;
 
+    private final static String FILE_NAME = "showers.json";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         showers = new ArrayList<>();
         recyclerView = findViewById(R.id.recycleView);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         getShowers();
         setRecyclerView(showers);
     }
@@ -72,20 +89,95 @@ public class MainActivity extends AppCompatActivity {
 
     public void getShowers(){
         try {
-            JSONObject obj = new JSONObject(loadJSONFromAsset());
-            JSONArray showersJSON = obj.getJSONArray("showers");
-            for(int i = 0; i < showersJSON.length(); i++) {
-                JSONObject jsonObject = showersJSON.getJSONObject(i);
+            File JSONfile = new File(getExternalFilesDir(null).getPath(), FILE_NAME);
+            JSONfile.getParentFile().mkdirs();
+            JSONfile.createNewFile();
+            FileInputStream in = new FileInputStream(JSONfile);
+            JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+            Log.d("READING", reader.peek().toString());
+            showers = readShowersArray(reader);
+            reader.close();
+            in.close();
+//            reader.beginObject();
 
-                Shower shower = new Shower();
-                shower.setId(jsonObject.getInt("id"));
-                shower.setDate(jsonObject.getString("date"));
-                shower.setLength(jsonObject.getInt("length"));
-
-                showers.add(shower);
-            }
-        } catch (JSONException | ParseException e) {
+//            JSONObject obj = new JSONObject(loadJSONFromAsset());
+//            JSONArray showersJSON = obj.getJSONArray("showers");
+//            for(int i = 0; i < showersJSON.length(); i++) {
+//                JSONObject jsonObject = showersJSON.getJSONObject(i);
+//
+//                Shower shower = new Shower();
+//                shower.setId(jsonObject.getInt("id"));
+//                shower.setDate(jsonObject.getString("date"));
+//                shower.setLength(jsonObject.getInt("length"));
+//
+//                showers.add(shower);
+//            }
+        } catch ( ParseException | IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<Shower> readShowersArray(JsonReader reader) throws IOException, ParseException {
+        List<Shower> showers = new ArrayList<>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            showers.add(readShower(reader));
+        }
+        reader.endArray();
+        return showers;
+    }
+
+
+    public Shower readShower(JsonReader reader) throws IOException, ParseException {
+        String date = null;
+        Long length = new Long(0);
+        int id = -1;
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("id")) {
+                id = reader.nextInt();
+            } else if (name.equals("length")) {
+                length = reader.nextLong();
+            } else if (name.equals("date")) {
+                date = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+
+        Shower shower = new Shower();
+        shower.setId(id);
+        shower.setDate(date);
+        shower.setLength(length);
+
+        return shower;
+    }
+
+    private String read(Context context, String fileName) {
+        try {
+            FileInputStream fis = context.openFileInput(fileName);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (FileNotFoundException fileNotFound) {
+            return null;
+        } catch (IOException ioException) {
+            return null;
+        }
+    }
+
+    public boolean isFilePresent(Context context, String fileName) {
+        String path = context.getFilesDir().getAbsolutePath() + "/" + fileName;
+        File file = new File(path);
+        return file.exists();
     }
 }
--- app/src/main/java/com/example/android/showerrush/ShowerActivity.java
+++ app/src/main/java/com/example/android/showerrush/ShowerActivity.java
@@ -1,17 +1,47 @@
 package com.example.android.showerrush;
 
+import android.content.Context;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonWriter;
 import android.util.Log;
 import android.view.View;
 import android.widget.Chronometer;
 
+import com.example.android.showerrush.model.Shower;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ShowerActivity extends AppCompatActivity {
+
+    public int id;
 
     private Chronometer chronometer;
     private boolean running;
     private long pauseOffset;
+
+    private final static String FILE_NAME = "showers.json";
+    private final static String TAG = "showeract";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +74,63 @@ public class ShowerActivity extends AppCompatActivity {
 
     }
     public void resetChrono(View view){
-
         chronometer.setBase(SystemClock.elapsedRealtime());
         pauseOffset = 0;
     }
 
-    public void finish(View view){
+    public void finish(View view) throws ParseException, IOException {
+        long length = SystemClock.elapsedRealtime() - chronometer.getBase();
+        Date date = GregorianCalendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy hh:mm");
+        String strDate = dateFormat.format(date);
 
+        Shower shower = new Shower();
+        shower.setLength(length);
+        shower.setDate(strDate);
+        shower.setId(3);
+
+        List<Shower> showers = new LinkedList<>();
+        showers.add(shower);
+
+        saveShower(showers);
+    }
+
+    public void writeShowersArray(JsonWriter writer, List<Shower> showers) throws IOException {
+        writer.beginArray();
+        for (Shower shower : showers) {
+            writeShower(writer, shower);
+        }
+        writer.endArray();
+    }
+
+
+    public void saveShower(List<Shower> showers)throws IOException{
+            File JSONfile = new File(getExternalFilesDir(null).getPath(), FILE_NAME);
+            JSONfile.getParentFile().mkdirs();
+            JSONfile.createNewFile();
+            OutputStream out = new FileOutputStream(JSONfile);
+            JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
+            writeShowersArray(writer, showers);
+            writer.close();
+            writer.flush();
+            out.close();
+
+//        Path filepath = Paths.get(getExternalFilesDir(null).getPath(), FILE_NAME);
+//        try {
+//            JsonWriter writer = new JsonWriter(new FileWriter(filepath.toFile()));
+//            GSON gson
+//            gson.toJson(shower, Shower.class, writer);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public void writeShower(JsonWriter writer, Shower shower) throws IOException {
+        writer.beginObject();
+        writer.name("id").value(shower.getId());
+        writer.name("length").value(shower.getLength());
+        writer.name("date").value(shower.getStrDate());
+        writer.endObject();
     }
 
 }
